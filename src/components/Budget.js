8192da9f9c85bec5e6ce7f224a1e3e38bd84c007
Ariel Slonimsqui <arislonimsqui@gmail.com>
2024-01-25 23:05:52 -0500

    changes made from theia lab environment

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -4,14 +4,33 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const updatedBudget = event.target.value;
+
+        if (updatedBudget > 20000) {
+            alert(`The new budget can not be higher than £20000`);
+        } 
+        
+        else if (updatedBudget < 960) {
+            alert(`The new budget can not be lower than the spending`);
+        } 
+        else {
+            setNewBudget(updatedBudget);
+        }
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £{budget}</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+            />
+        </div>
     );
 };
+
 export default Budget;
